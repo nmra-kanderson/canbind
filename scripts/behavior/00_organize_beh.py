426c05e3f1c0c87ed723ad3e5b8f26edc3baea8a
Kevin Anderson <kevin.anderson@neumoratx.com>
2022-09-08 18:11:28 -0400

    update

--- scripts/behavior/00_organize_beh.py
+++ scripts/behavior/00_organize_beh.py
@@ -6,12 +6,14 @@ from pathlib import Path
 
 
 # Set up directories
-base_dir     = '/home/ubuntu/canbind-fsx'
+base_dir   = '/home/ubuntu/canbind-fsx'
 clin_dir   = '/home/ubuntu/fsx/Clinical'
 
 scale_list = [
-    'ATHF','BMI','BRIAN','CNSVS','DARS','DID','GAD7','IPAQ','MADRS','MINI','PSQI','QIDS','SDS','SHAPS','WHOQOL',
-    'BISBAS','BPI','CGI','DEMO','ECRR','HCL32','LEAPS','MEDHIS','NEOFFI','PSYHIS','QLESQ','SEXFX','SPAQ','YMRS'
+    'ATHF','BMI','BRIAN','CNSVS','DARS','DID','GAD7',
+    'IPAQ','MADRS','MINI','PSQI','QIDS','SDS','SHAPS','WHOQOL',
+    'BISBAS','BPI','CGI','DEMO','ECRR','HCL32','LEAPS',
+    'MEDHIS','NEOFFI','PSYHIS','QLESQ','SEXFX','SPAQ','YMRS'
 ]
 
 sessions_dir = Path(base_dir, 'research/imaging/datasets/SRPBS/processed_data/pf-pipelines/qunex-nbridge/studies/CANBIND-20220818-mCcU5pi4/sessions')
